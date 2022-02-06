0267c408ad425e6ded76d1914d3d2e75e61b250c
V_The_Titan_Of_Programming <wizardy.oni@gmail.com>
2022-02-06 19:54:54 +0300

    Добавил EEPROM, пофиксил баг с некорректым переливанием

--- SVET_TEST/main.cpp
+++ SVET_TEST/main.cpp
@@ -5,38 +5,37 @@
 * Author : Pumpkin
 */
 
-#define F_CPU 20000000UL
+#define F_CPU 20000000UL //20MHz
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdlib.h>
 
 #include "lib/core/core.h"
-//#include "lib/core/stdafx.h"
+
 
 #define DIN pinB2
 #define IMP pinA7
 #define BTN pinA3
-#define ADC_1 pinA5
 
 uint8_t base_ADC_level = 0;
-//static bool b_BTN = true;
 
-void set_H_and_delay()	{
+
+void set_H_and_delay()	{ //send H bit
 	PORTB |= 0b00000100;
 	_delay_us(1);
 	PORTB &= 0b11111011;
 }
 
-void set_L_and_delay()	{
+void set_L_and_delay()	{ //send L bit
 	PORTB |= 0b00000100;
 	PORTB &= 0b11111011;
 	_delay_us(1);
 }
 
-void PWM_generation() {	   //8-bit
+void PWM_generation() {	   //8-bit PWM for reading the button status
 	TCCR0A = 0b00100011; //COM0B1 = 0, COM0B0 = 1; WGM01 = 0, WGM00 = 1; (phase-corrent PWM on A7)
-	TCCR0B = 0b00000001; // CLK (need more info on frequency)
+	TCCR0B = 0b00000001; // CLK
 	TCNT0=0; //counter starting value
 	OCR0B=127; //PWM duty
 }
@@ -44,7 +43,7 @@ void PWM_generation() {	   //8-bit
 
 
 
-void EEPROM_write(uint8_t data, uint8_t address/*, something else? */) {
+void EEPROM_write(uint8_t data, uint8_t address) {
 	/* Wait for completion of previous write */
 	while(EECR & (1<<EEPE));
 	
@@ -77,7 +76,7 @@ uint8_t EEPROM_read(uint8_t address)
 	return EEDR;
 }
 
-void send_byte(unsigned char b) {
+void send_byte(unsigned char b) { // compare the HEX value of color to send the corresponding bits
 	if (b & 0b10000000) {
 		set_H_and_delay();
 	}
@@ -128,7 +127,7 @@ void send_byte(unsigned char b) {
 	}
 }
 
-void send_GRB(uint8_t g, uint8_t r, uint8_t b){
+void send_GRB(uint8_t g, uint8_t r, uint8_t b){ //send the current colors in the following order - green, red, blue (the value is HEX)
 	for (uint8_t i = 0; i < 144; i++){
 		send_byte (g);
 		send_byte (r);
@@ -139,49 +138,30 @@ void send_GRB(uint8_t g, uint8_t r, uint8_t b){
 
 
 void ADC_init(){ //ADC on A2
-	ADMUX = 0b00000010; //check the reference voltage or the VCC ???????????????????????????
-	ADCSRA = 0b10000111; //CLK/128 (~?kHz)
+	ADMUX = 0b00000010; //check the reference voltage or the VCC
+	ADCSRA = 0b10000111; //CLK/128
 	ADCSRB = 0b00010000; // left-shift ADCH for 8-bit value
 }
 
-void base_ADC_measure(){
+void base_ADC_measure(){ //set the average adc level for fair measurement
 	
 	uint16_t current_ADC_level = 0;
 	uint8_t adc_lvl = 0;
-	//uint8_t base_ADC_level = 0;
-	//ADCSRA |= (1 << ADSC);
-	for (uint8_t i = 0; i < 100; i++)
+	for (uint8_t i = 0; i < 100; i++) //measure current ADC level 100 times
 	{
 		ADCSRA |= (1 << ADSC);
 		while (ADCSRA & (1 << ADSC) );
 		adc_lvl = ADCH;
-		//_delay_ms(100);
-		//current_ADC_level += (uint16_t)(adc_lvl);
-		//if (adc_lvl >= 200){
-		//send_GRB(255,0,0);
-		//_delay_ms(10);
-		//send_GRB(255,255,0);
-		//_delay_ms(10);
-		//}
-		//else {
-		//send_GRB(0,0,0);
-		//_delay_ms(10);
-		//send_GRB(255,255,255);
-		//_delay_ms(10);
-		//}
-
 		current_ADC_level += (uint16_t)(adc_lvl);
 	}
-	current_ADC_level = current_ADC_level / 100u;
-	current_ADC_level = current_ADC_level * 95u;
+	current_ADC_level = current_ADC_level / 100u; //find the average value of base 
+	current_ADC_level = current_ADC_level * 95u; //set the base to 95% of the average
 	current_ADC_level = current_ADC_level / 100u;
 	::base_ADC_level = (uint8_t)(current_ADC_level);
-	//::base_ADC_level = 202;
 
-	//return base_ADC_level;
 }
 
-void PWM_generation_16() {	   //16-bit
+void PWM_generation_16() {	   //16-bit PWM for testing purposes
 	TCCR1A = 0b00100001; //COM0B1 = 0, COM0B0 = 1; WGM01 = 0, WGM00 = 1; (phase-corrent PWM on A7)
 	TCCR1B = 0b00000001; // CLK (need more info on frequency)
 	//TCCR1C = 0b01000000; // CLK (need more info on frequency)
@@ -191,7 +171,7 @@ void PWM_generation_16() {	   //16-bit
 	OCR1BL=ADCH; //PWM duty
 }
 
-bool get_button_status(){
+bool get_button_status(){ //read the current button status
 	ADCSRA |= (1 << ADSC);
 	while (ADCSRA & (1 << ADSC) );
 	if(ADCH <= ::base_ADC_level){
@@ -207,14 +187,14 @@ main()
 {
 	pinMode(DIN, OUTPUT);
 	pinMode(IMP, OUTPUT);
-	//pinMode(ADC_1, OUTPUT);
+
 	pinMode(BTN, INPUT);
 	digitalWrite(BTN, HIGH);
 	ADC_init();
 	PWM_generation();
 	base_ADC_measure();
 
-	//uint8_t brt = 0,stage = 0, cur_g = 0, cur_r = 0, cur_b = 0;
+
 	int8_t dir = 1;
 	bool btn_status = false, btn_press = false, turn_off = false;
 
@@ -224,20 +204,17 @@ main()
 	uint8_t stage = EEPROM_read(3);
 	uint8_t brt = EEPROM_read(4);
 
-	int timebase = 0, cur_stage_time = 0;
-	//_delay_ms(10);
-	//send_GRB(255,255,255);
-	//_delay_ms(10);
-	//send_GRB(cur_g,cur_r,cur_b);
+	int timebase = 0, cur_stage_time = 0, last_stage_time = 0;
+
 	while (1)
 	{
-		//PWM_generation_16();
+
 		
 		if (get_button_status())	{
 			
 			timebase++;
-			//digitalWrite(IMP, HIGH);
-			if (timebase >= 50 && timebase < 1000 && btn_press == false){
+
+			if (timebase >= 50 && timebase < 1000 && btn_press == false){ //turning on the lights
 				
 				if (btn_status == false){
 					
@@ -247,7 +224,7 @@ main()
 					turn_off = false;
 					_delay_ms(100);
 				}
-				else {
+				else { //turning off the lights
 					send_GRB(0,0,0);
 					EEPROM_write(cur_g, 0);
 					EEPROM_write(cur_r, 1);
@@ -260,11 +237,12 @@ main()
 					_delay_ms(100);
 				}
 			}
-			else if (timebase >= 1000 && turn_off == false)
+			else if (timebase >= 1000 && turn_off == false) //start the color changing procedure
 			{
-				//if (cur_stage_time != 0){
-				//timebase += cur_stage_time;
-				//}
+				if (last_stage_time != 0){
+				timebase += last_stage_time;
+				last_stage_time = 0;
+				}
 				
 				brt += dir;
 				if (brt >= 255) {
@@ -275,7 +253,7 @@ main()
 					
 					dir = 1;
 				}
-
+				cur_stage_time++;
 				// time the animation
 				if (timebase > 1509) {
 					cur_stage_time=0;
@@ -288,63 +266,63 @@ main()
 						stage = 0;
 					}
 				}
-				//send_GRB(0,0,255-ADCH);
 
-				if (stage == 0) {
+
+				if (stage == 0) { //red
 					send_GRB(0,brt,0);
 					cur_g = 0;
 					cur_r = brt;
 					cur_b = 0;
-					cur_stage_time++;
-					} else if (stage == 1) {
+
+					} else if (stage == 1) { //orange
 					send_GRB(brt / 2,brt,0);
 					cur_g = brt / 2;
 					cur_r = brt;
 					cur_b = 0;
-					cur_stage_time++;
-					} else if (stage == 2) {
+
+					} else if (stage == 2) {//yellow
 					send_GRB(brt,brt,0);
 					cur_g = brt;
 					cur_r = brt;
 					cur_b = 0;
-					cur_stage_time++;
-					} else if (stage == 3) {
+
+					} else if (stage == 3) {//green
 					send_GRB(brt,0,0);
 					cur_g = brt;
 					cur_r = 0;
 					cur_b = 0;
-					cur_stage_time++;
-					} else if (stage == 4) {
+
+					} else if (stage == 4) {//light blue
 					send_GRB(brt,0, brt);
 					cur_g = brt;
 					cur_r = 0;
 					cur_b = brt;
-					cur_stage_time++;
-					} else if (stage == 5) {
+					} else if (stage == 5) {// blue
 					send_GRB(0, 0, brt);
 					cur_g = 0;
 					cur_r = 0;
 					cur_b = brt;
-					cur_stage_time++;
-					} else if (stage == 6) {
+
+					} else if (stage == 6) {//purple
 					send_GRB(0, brt / 2, brt);
 					cur_g = 0;
 					cur_r = brt / 2;
 					cur_b = brt;
-					cur_stage_time++;
-					} else if (stage == 7) {
+
+					} else if (stage == 7) {//white
 					send_GRB(brt, brt, brt);
 					cur_g = brt;
 					cur_r = brt;
 					cur_b = brt;
-					cur_stage_time++;
+
 				}
 			}
 		}
 		else {
-			//btn_status = false;
+
 			btn_press = false;
 			timebase = 0;
+			last_stage_time = cur_stage_time;
 		}
 		_delay_ms(1);
 	}
